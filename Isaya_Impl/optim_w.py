4cadde888565a5d4b708c2df0ac63558c7149b75
Isaya <isaya.escande@telecom-paris.fr>
2024-06-14 15:01:02 +0200

    Clean WOptim class

--- Isaya_Impl/optim_w.py
+++ Isaya_Impl/optim_w.py
@@ -10,19 +10,16 @@ class WOptimizer:
     def objective(self, W_flat, A):
         # Reshape W_flat to matrix W
         W = W_flat.reshape(A.shape[0], A.shape[1])
-        # Calculate the norm of W.T @ A
         norm_value = np.linalg.norm(W.T @ A)
         return norm_value
 
     def constraint(self, W_flat, A):
-        # Reshape W_flat to matrix W
         W = W_flat.reshape(A.shape[0], A.shape[1])
         # Constraint for each row i: W[i].T @ A[i] == 1
         constraints = [W[i] @ A[i] - 1 for i in range(A.shape[0])]
         return np.array(constraints)
     
     def callback(self, W_flat):
-        # Reshape W_flat to matrix W
         self.iteration += 1
         W = W_flat.reshape(self.A.shape[0], self.A.shape[1])
         print(f"Norm of W.T@A at iteration {self.iteration} : {np.linalg.norm(W.T @ self.A)}")
@@ -36,7 +33,6 @@ class WOptimizer:
         # Constraints
         cons = [{'type': 'eq', 'fun': self.constraint, 'args': (A,)}]
 
-        # Minimize
         result = minimize(self.objective, W_initial, args=(A,), constraints=cons, method='SLSQP',callback=self.callback)
 
         # Reshape the solution back to matrix form
